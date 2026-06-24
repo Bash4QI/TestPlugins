014fd6274dfa3221146c26198ed6c31da0b170e1
Bash4QI <99900010+Bash4QI@users.noreply.github.com>
2026-06-25 01:45:00 +0300

    Update build.gradle.kts

--- ExAr/build.gradle.kts
+++ ExAr/build.gradle.kts
@@ -1,24 +1,3 @@
-plugins {
-    id("com.android.library")
-    id("kotlin-android")
-    id("com.recloudstream.gradle")
-}
-
-android {
-    compileSdk = 33
-    defaultConfig {
-        minSdk = 21
-        targetSdk = 33
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
-}
-
 dependencies {
     compileOnly("com.lagradost:cloudstream3:pre-release")
     implementation("com.github.Blatzar:NiceHttp:0.4.4")
