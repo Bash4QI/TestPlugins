162b96b72451769c355280244af22b57ab44fec4
Bash4QI <99900010+Bash4QI@users.noreply.github.com>
2026-06-25 01:00:28 +0300

    Add initial configuration for cloudstream plugin

---
+++ ExAr/build.gradle.kts
@@ -0,0 +1,8 @@
+cloudstream {
+    description = "My Test Plugin"
+    authors = listOf("bash4qi")
+    status = 1
+    tvTypes = listOf("Movie", "TvSeries")
+    language = "en"
+    requiresResources = false
+}
