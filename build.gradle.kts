794ca6041dda0f2ee19c82c78eadf5cdb2f3d133
Bash4QI <99900010+Bash4QI@users.noreply.github.com>
2026-06-25 01:44:28 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ buildscript {
     dependencies {
         classpath("com.android.tools.build:gradle:7.0.4")
         classpath("com.github.recloudstream:gradle:-SNAPSHOT")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10") // غيرنا الإصدار
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
     }
 }
 
@@ -16,5 +16,26 @@ allprojects {
         google()
         mavenCentral()
         maven("https://jitpack.io")
+    }
+}
+
+subprojects {
+    apply(plugin = "com.android.library")
+    apply(plugin = "kotlin-android")
+    apply(plugin = "com.recloudstream.gradle")
+
+    android {
+        compileSdk = 33
+        defaultConfig {
+            minSdk = 21
+            targetSdk = 33
+        }
+        compileOptions {
+            sourceCompatibility = JavaVersion.VERSION_1_8
+            targetCompatibility = JavaVersion.VERSION_1_8
+        }
+        kotlinOptions {
+            jvmTarget = "1.8"
+        }
     }
 }
