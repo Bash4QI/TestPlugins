f55976347e2a18b6572dae5344a18829cbe781c9
Bash4QI <99900010+Bash4QI@users.noreply.github.com>
2026-06-25 02:04:50 +0300

    Update build.gradle.kts

--- ExAr/build.gradle.kts
+++ ExAr/build.gradle.kts
@@ -1,3 +1,8 @@
+import com.lagradost.cloudstream3.gradle.CloudstreamExtension
+
+fun Project.cloudstream(configuration: CloudstreamExtension.() -> Unit) = 
+    extensions.getByName<CloudstreamExtension>("cloudstream").configuration()
+
 cloudstream {
     pluginVersion = 1
     description = "My Custom Arabic Extension"
