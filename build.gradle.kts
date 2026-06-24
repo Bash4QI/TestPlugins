4ae809464dce89b76d40b34a2141177d89e61055
Bash4QI <99900010+Bash4QI@users.noreply.github.com>
2026-06-25 01:32:16 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,28 +26,16 @@ subprojects {
 
     android {
         compileSdk = 33
-
         defaultConfig {
             minSdk = 21
             targetSdk = 33
         }
-
         compileOptions {
             sourceCompatibility = JavaVersion.VERSION_1_8
             targetCompatibility = JavaVersion.VERSION_1_8
         }
-
         kotlinOptions {
             jvmTarget = "1.8"
         }
-    }
-}
-
-    dependencies {
-        compileOnly("com.lagradost:cloudstream3:pre-release")
-        implementation("com.github.Blatzar:NiceHttp:0.4.4")
-        implementation("org.jsoup:jsoup:1.16.2")
-        implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.16.0")
-        implementation("com.fasterxml.jackson.core:jackson-databind:2.16.0")
     }
 }
