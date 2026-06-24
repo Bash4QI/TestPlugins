11f2d4ddd99bfb152fa70d3a22b8d5b3e8cd5e8a
Bash4QI <99900010+Bash4QI@users.noreply.github.com>
2026-06-25 01:28:17 +0300

    Update build.gradle.kts

--- ExAr/build.gradle.kts
+++ ExAr/build.gradle.kts
@@ -1,3 +1,11 @@
+dependencies {
+    compileOnly("com.lagradost:cloudstream3:pre-release")
+    implementation("com.github.Blatzar:NiceHttp:0.4.4")
+    implementation("org.jsoup:jsoup:1.16.2")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.16.0")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.16.0")
+}
+
 cloudstream {
     description = "My Test Plugin"
     authors = listOf("bash4qi")
