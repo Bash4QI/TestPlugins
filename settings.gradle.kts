62bb8aadbf2952d19b6ac64961fc221f12b8d9a2
Bash4QI <99900010+Bash4QI@users.noreply.github.com>
2026-06-25 01:49:07 +0300

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,9 +1,6 @@
-rootProject.name = "TestPlugins"
+rootProject.name = "CloudstreamPlugins"
 
-// This file sets what projects are included.
-// All new projects should get automatically included unless specified in the "disabled" variable.
-
-val disabled = listOf<String>()
+val disabled = listOf<String>("ExampleProvider")
 
 File(rootDir, ".").eachDir { dir ->
     if (!disabled.contains(dir.name) && File(dir, "build.gradle.kts").exists()) {
@@ -14,6 +11,3 @@ File(rootDir, ".").eachDir { dir ->
 fun File.eachDir(block: (File) -> Unit) {
     listFiles()?.filter { it.isDirectory }?.forEach { block(it) }
 }
-
-// To only include a single project, comment out the previous lines (except the first one), and include your plugin like so:
-include(":ExAr")
